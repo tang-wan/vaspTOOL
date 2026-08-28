9647ad4cef74201bbc1521288ef84dd063b32ff4
TangTang <a5511572@gmail.com>
2026-08-29 02:06:09 +0800

    Add _Info_() fumction in poscarTOOL_v2, and update the color

--- poscarTOOL_v2.py
+++ poscarTOOL_v2.py
@@ -1,24 +1,27 @@
 import numpy as np # type: ignore
 from STT_Tool import Check_out_Word as CoW
+from STT_Tool import Process_Word as pw
 import ase.io as ase
 from collections import Counter
 from itertools import groupby
 
+
+def _Info_POSCARRead():
+    print("=====")
+    print("We have the following methods:")
+    methodList = ("LayerHeight", "AtomDistance", "AtomAngle")
+    for name in methodList:
+        print(f"    {name}") 
+    print("=====") 
+# -----
 class POSCARRead():
     def __init__(self, path, p=False):
-        print("=====")
-        print("We have the following methods:")
-        print("     LayerHeight\n"   \
-              "     AtomDistance \n" \
-              "     AtomAngle"
-              )
-        print("=====")
         Struct = ase.read(path)
 
         Formula = Struct.get_chemical_formula()
         Symbols = Struct.get_chemical_symbols()
         # print(Symbols)
-        print(f"This POSCAR/CONTCAR is {Formula}")
+        CoW(f"This POSCAR/CONTCAR is {Formula}")
         print("---")
         Vector_a1 = Struct.get_cell()[0]
         Vector_a2 = Struct.get_cell()[1]
@@ -28,13 +31,13 @@ class POSCARRead():
                                  Vector_a2, 
                                  Vector_a3]
                                   )
-        print("Lattice Vector:")
+        pw("Lattice Vector:")
         print(LatticeVector)
         print("---")
         Constant_a1 = np.sqrt(Vector_a1[0]**2+Vector_a1[1]**2+Vector_a1[2]**2)
         Constant_a2 = np.sqrt(Vector_a2[0]**2+Vector_a2[1]**2+Vector_a2[2]**2)
         Constant_a3 = np.sqrt(Vector_a3[0]**2+Vector_a3[1]**2+Vector_a3[2]**2)
-        print("Lattice Constant:")
+        pw("Lattice Constant:")
         print(Constant_a1, Constant_a2, Constant_a3)
         print("---")
         
@@ -42,14 +45,13 @@ class POSCARRead():
         AtomPos_Cart = Struct.get_positions()
         AtomPos_Frac = Struct.get_scaled_positions()
         if p:
-            print("Atom Position (Cartesian):")
+            pw("Atom Position (Cartesian):")
             print(AtomPos_Cart)
             print("---")
-            print("Atom Position (Fractional):")
+            pw("Atom Position (Fractional):")
             print(AtomPos_Frac)
             print("---")
         # ---
-        print("=====")
         self.Struct = Struct
         self.Symbols = Symbols
         self.Vector_a1, self.Vector_a2, self.Vector_a3 = Vector_a1, Vector_a2, Vector_a3
@@ -94,19 +96,21 @@ class POSCARRead():
         # Sorted_AtomPos_dz = Sorted_AtomPos_dz[Sorted_AtomPos_dz!=0]
         # Sorted_AtomPos_dz = [key for key, group in groupby(Sorted_AtomPos_dz)]
 
-        print("LayerHeight (Å):")
+        pw("LayerHeight (Å):")
         print(Sorted_AtomPos_z)
         print("----")
-        print("LayerHeight difference (Å):")
+        pw("LayerHeight difference (Å):")
         print(Sorted_AtomPos_dz)
+        print("----")
     
     def AtomDistance(self, TargetAtom_1, TargetAtom_2, p=(False, False)): # Atom2 - Atom1
         self._ReadPos(TargetAtom_1, TargetAtom_2, p=p)
 
         AtomDist = self.Struct.get_distance(TargetAtom_1, TargetAtom_2, mic=True)
 
-        print(f"Atom Distance ({self.Symbols[TargetAtom_1]} - {self.Symbols[TargetAtom_2]}):")
+        pw(f"Atom Distance ({self.Symbols[TargetAtom_1]} - {self.Symbols[TargetAtom_2]}):")
         print(f"{AtomDist:.6f} Å")
+        print("----")
 
         return AtomDist
 
@@ -116,29 +120,32 @@ class POSCARRead():
 
         angle = self.Struct.get_angle(TargetAtom_1, TargetAtom_2, TargetAtom_3, mic=True)
         
-        print(f"The angle ({self.Symbols[TargetAtom_1]}-{self.Symbols[TargetAtom_2]}-{self.Symbols[TargetAtom_3]}) is:")
+        pw(f"The angle ({self.Symbols[TargetAtom_1]}-{self.Symbols[TargetAtom_2]}-{self.Symbols[TargetAtom_3]}) is:")
         print(f"{angle:.3f} degree")
+        print("----")
         
         return angle
 
 # ===========================================
-
+def _Info_POSCAREdit():
+    print("=====")
+    print("We have the following methods:")
+    methodList = ("SetVacCen",
+                  "Centered",
+                  "SuperCell",
+                  "TF_repeat",
+                  "LatticeStrain",
+                  "LatticeSet",
+                  "HeteroStructure",
+                  "WritePOSCAR",
+                  "Write_Atom_xyz"
+                    )
+    for name in methodList:
+        pw(f"    {name}") 
+    print("=====") 
+# -----
 class POSCAREdit():
     def __init__(self, path, p=False):
-        print("=====")
-        print("We have the following methods:")
-        print("     SetVacCen\n" \
-              "     Centered\n" \
-              "     SuperCell\n" \
-              "     TF_repeat\n" \
-              "     LatticeStrain\n" \
-              "     LatticeSet\n" \
-              "     HeteroStructure\n" \
-              "     -----\n" \
-              "     WritePOSCAR\n" \
-              "     Write_Atom_xyz\n" \
-                )
-        print("=====")
         Struct = ase.read(path)
         if 'momenta' in Struct.arrays:
             del Struct.arrays['momenta']
@@ -146,7 +153,7 @@ class POSCAREdit():
         Formula = Struct.get_chemical_formula()
         Symbols = Struct.get_chemical_symbols()
         # print(Symbols)
-        print(f"This POSCAR/CONTCAR is {Formula}")
+        CoW(f"This POSCAR/CONTCAR is {Formula}")
         print("---")
         Vector_a1 = Struct.get_cell()[0]
         Vector_a2 = Struct.get_cell()[1]
@@ -169,15 +176,14 @@ class POSCAREdit():
         AtomNum = len(Struct)
         AtomPos_Cart = Struct.get_positions()
         AtomPos_Frac = Struct.get_scaled_positions()
-        if p:
-            print("Atom Position (Cartesian):")
-            print(AtomPos_Cart)
-            print("---")
-            print("Atom Position (Fractional):")
-            print(AtomPos_Frac)
-            print("---")
+        # if p:
+        #     print("Atom Position (Cartesian):")
+        #     print(AtomPos_Cart)
+        #     print("---")
+        #     print("Atom Position (Fractional):")
+        #     print(AtomPos_Frac)
+        #     print("---")
         # ---
-        print("=====")
         self.Struct = Struct
         self.Symbols = Symbols
         self.LatticeVector = LatticeVector
@@ -250,7 +256,9 @@ class POSCAREdit():
         hetero.center(vacuum=Vac, axis=2)
         
         self.Struct = hetero
-        print(f"Heterostructure built! Top layer loaded from: {top_path}")
+        pw(f"Heterostructure built! Top layer loaded from:")
+        print(f"     {top_path}")
+        print("---")
 # ==========================
     def _get_sorted_struct(self):
         unique_symbols = []
@@ -272,7 +280,9 @@ class POSCAREdit():
                 format='vasp', 
                 vasp5=True, 
                 direct=True)
-        print(f"Writing POSCAR Finished: {name}")
+        pw(f"Writing POSCAR Finished:")
+        print(f"    {name}")
+        print("=====")
 
     def Write_Atom_xyz(self, Path, r, theta, phi):            
         # 使用已經完美排序過的結構
@@ -286,7 +296,7 @@ class POSCAREdit():
         # 保持原本的元素順序
         unique_symbols = list(dict.fromkeys(AtomType)) 
 
-        print("Start to convert vasp POSCAR to nanodcal atom.xyz")
+        pw("Start to convert vasp POSCAR to nanodcal atom.xyz")
         
         with open(Path, "w") as W_File:
             W_File.write(f"{len(Cart_AtomPos)}\n")
@@ -301,5 +311,6 @@ class POSCAREdit():
                     W_File.write(f"{atom:<8}{Cart_AtomPos[pos][0]:>20.14f}{Cart_AtomPos[pos][1]:>20.14f}{Cart_AtomPos[pos][2]:>20.14f}{r[i]:^30.14f}{theta[i]:^30.14f}{phi[i]:^30.14f}\n")
                     pos += 1
             
-        print("Converting Finished.")
+        pw("Converting Finished.")
+        print("=====")
 # =====
